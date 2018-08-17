73a72aab865f015e49c79b5d4b3905454fb31523
Tom Betthauser <tombetthauser@gmail.com>
2018-08-17 10:39:54 -0700

    added redirect function and kernal page redirect

---
+++ app.js
@@ -0,0 +1,22 @@
+var fSpeed = true;
+var tFast = (Math.random() * 10000) + 1000;
+var tSlow = (Math.random() * 60000) + 10000;
+var r = "http://www.tombetthauser.com/kernal"
+var t;
+
+if(fSpeed === true){
+	t =+ tFast;
+} else {
+	t =+ tSlow;
+}
+
+window.onkeyup = function(e) {
+   var key = e.keyCode ? e.keyCode : e.which;
+   if (key == 13) {
+       window.location = r;
+   }
+}
+
+setInterval(function(){
+	window.location = r;
+}, t);
