753fb4575857b6d470bf852cd59ea09ab4384179
Tom Betthauser <tombetthauser@gmail.com>
2018-09-27 13:18:27 -0700

    Overhaul Rebuild to JS Template

---
+++ assets/scripts/script.js
@@ -0,0 +1,161 @@
+var displayNames = true;
+
+var artist = {
+	name: {
+		first: 'Tom',
+		last: 'Betthauser'
+	},
+	work: [
+		'VD03-FA16, all models / textures / sound produced by artist in collaboration Bret Ward, 2016 (<a target="_blank" href="https://drive.google.com/file/d/0B0i92gIfB6BKcDNBLXBqSHRkaUk/view">video link</a>)',
+		'Installation shot, San Francisco Art Institute, 3rd St. Studio Fellowship (program link), 2016',
+		'Cleric_0.2[cast1], 4"x 5" concrete cast of original sculpture / hand-woven mat, 2016',
+		'Cleric(s)_0.2, 4"x 5" concrete cast & original sculpture (left) / hand-woven mats, 2016',
+		'Sketchbook selection (1 of 50), 8.5"x11" inkjet print scanned from ballpoint sketch, 2016',
+		'Sketchbook selection (1 of 50), 8.5"x11" inkjet print scanned from ballpoint sketch, 2016',
+		'157s.Cat_1[study], 4"x 5" watercolor / paper, 2016',
+		'MIRc.4.5bya[study], 4"x 5" watercolor / gouache / paper, 2016',
+		'Exhibition shot, Anchorage (AK), framed watercolors / sketchbook prints / digital video projection, 2016.',
+		'Vaab_LITE[study]-SM16, 5"x 4" watercolor / paper, 2016',
+		'BK01-SM16 (book of drawings), 4.25"x 5.5" published in Oakland CA, 2016.',
+		'Group exhibition curated by Christopher Ford, CB1 Gallery, Los Angeles, 2016 (exhibition link)',
+		'KEEN-SP16, 7"x 7.25" oil / poplar, 2015/16',
+		'ALEO-1314 (Bird in Space), 6"x 6.5" oil / acrylic / birch ply, 2014',
+		'DIOT-FA15, 7.75"x 7.5"  oil / poplar panel, 2015',
+		'HION-1314, 5.75"x 4.5" oil / birch panel, 2014',
+		'Group exhibition curated by Brett Reichman, CB1 Gallery, Los Angeles, 2016 (exhibition link)',
+		'GTS1-SP16 (Gates), 45"x 77"  graphite / paper, 2015/16',
+		'AMR1-1314 (Amar I), 12"x 19" graphite / paper, 2014',
+		'AMR2-1314 (Amar II), 12"x 19" graphite / paper, 2014',
+		'Rkm2-SP15 (Stones II), 9"x 11"  graphite / mixed media frame, 2015',
+		'SRF1-SP15 (Surface), 7"x 8.5"  graphite / paper, 2015',
+		'CLER-SP15 (Cleric), 7"x 7.5"  graphite / paper, 2015',
+		'Rkm1-FA15 (Stones I), 15""x 19"  watercolor / corkboard, 2015',
+		'Solo exhibition / installation (exterior), LRC Gallery, Ridgecrest CA, 2014/15',
+		'Solo exhibition / installation (interior), LRC Gallery, Ridgecrest CA, 2014/15',
+		'BSTR-1314 (Natura), 6"x 6"  acrylic / oil / canvas, 2014',
+		'AMAN-1314, 6"x 7" oil / birch ply, 2014',
+		'BRUG-1314 (Cambria), 6"x 7" oil / birch ply, 2014',
+		'MAP1-1314, 6"x 5" oil / canvas, 2014',
+		'MEC1-1314, 6"x 7" oil / canvas, 2014',
+		'SALM-1314, 4"x 5.5" oil / canvas, 2014',
+		'BERK-1314, 6"x 5" oil / birch ply, 2014',
+		'CONA-1314 (Conan), 6"x 6" oil / canvas, 2014',
+		'MIM1-SP12 (Mimoid Diptych I), 70"x 48"graphite / paper, 2012',
+		'MIM1-SP12 (Mimoid Diptych II), 70"x 48"graphite / paper, 2012',
+		'LND2-SP12 (Land), 78"x 48" graphite / paper, 2011/12',
+		'Group exhibition, Green Gallery, Yale School of Art, 2012 (sculptures by Joyce Kalema)',
+		'Group exhibition, Green Gallery, Yale School of Art, 2012 (sculpture by Joyce Kalema)',
+		'ISL1-FA11, 32"x 16" graphite / paper, 2011',
+		'WAL1-FA11, 24"x 19" graphite / paper, 2011',
+		'EGPT-SP11, 24"x 19" graphite / paper, 2011',
+		'ISL2-FA11, 24"x 19" graphite / paper, 2011',
+		'LND1-SP10, 72"x 48" graphite / stretched paper, 2010/11',
+		'SATV-SM09, 24"x 19" graphite / paper, 2010',
+		'OBSR-SM09, 24"x 19" graphite / paper, 2010',
+		'TEMP-SM10, 24"x 19" graphite / paper, 2010',
+		'PURP-FA08, 12"x 9" acrylic / masonite, 2008',
+		'CRSH-SM08, 15"x 19" acrylic / masonite, 2008',
+		'OREG-WB07, 12"x 19" acrylic / masonite, 2007',
+		'SF01-FA07, 12"x 10" acrylic / birch ply, 2007',
+		'STV1-FA08 (Steve), 6"x 5.5" acrylic / panel, 2008',
+		'SFLT-FA08 (Daly City), 4"x 5" acrylic / panel, 2008',
+		'MRK2-FA08 (Mark), 6"x 5" acrylic / panel, 2008'
+	],
+	cv: [
+		'<h5>CONTACT:</h5>',
+		'tombetthauser@gmail.com',
+
+		'<h5>EDUCATION:</h5>',
+		'Yale University - Master of Fine Arts, 2012',
+		'San Francisco Art Institute - Bachelor of Fine Arts, 2010',
+		
+		'<h5>TEACHING EXPERIENCE:</h5>',
+		'College of Marin, CA – Adjunct Professor, Beginning & Advanced Painting, May 2018 – Present',
+		'West Valley College, San Jose CA – Adjunct Professor, Studio Art / Art History, 2017 – Present',
+		'University of California San Diego CA – Visiting Artist (lecture / studio visits) – Nov 2017',
+		'Wylie & May Louise Jones Gallery, Bakersfield CA – Chief Gallery Director / Curator, 2014 – 2017',
+		'Bakersfield College, CA – Adjunct Professor, Studio Art / Art History, 2014 – 2017',
+		'Cerro Coso Community College, CA – Adjunct Prof. Studio Art / Art History, 2013 – 2017',
+		'Artvoices Magazine, Los Angeles CA – Contributing Writer, 2014 – 2016',
+		'San Francisco Art Institute, CA – Teaching Assistant, 2007 – 2010',
+		'Exploratorium Museum, CA – Explainer / Tactile Dome / Public Programs, 2006 – 2012'
+	]
+}
+
+
+// script ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+loadNavbar();
+loadImages();
+loadTitle();
+loadCV();
+
+
+// functions ––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+function loadTitle() {
+	$("head").prepend(
+  		'<title>' + artist.name.first + ' ' + artist.name.last + '</title>'
+	)
+}
+
+function loadNavbar() {
+	$("body").prepend(
+	  	'<nav class="t-nav2-container navbar navbar-default hidden-lg hidden-md hidden-sm">'
+		+  '<div class="container-fluid" id="t-navbar-white">'
+		+    '<div class="navbar-header">'
+		+      '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">'
+		+         '<span class="sr-only">Toggle navigation</span>'
+		+         '<span class="icon-bar"></span>'
+		+         '<span class="icon-bar"></span>'
+		+         '<span class="icon-bar"></span>'
+		+       '</button>'
+		+       '<a class="navbar-brand container" href="../../index.html">' + artist.name.first + ' ' + artist.name.last + '</a>'
+		+     '</div>'
+		+     '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'
+		+       '<ul class="nav navbar-nav navbar-right">'
+		+       	'<li><a href="../../index.html" id="t-nav-link-mobile">&nbsp; home</a></li>'
+		+      		'<li><a href="images.html" id="t-nav-link-mobile">&nbsp; images</a></li>'
+		+      		'<li><a target="_blank" href="https://tombetthauser.bandcamp.com/" id="t-nav-link-mobile">&nbsp; music</a></li>'
+		+			'<li><a target="_blank" href="../../assets/pdfs/cv.pdf" id="t-nav-link-mobile">&nbsp; info</a></li>'
+		+       '</ul>'
+		+     '</div>'
+		+   '</div>'
+		+ '</nav>'
+	)
+}
+
+function loadImages() {
+	for(i = artist.work.length; i > 0 ; i--){
+		  $("#container").append(
+				"<a target='new' href='../../assets/images/portfolio/full/"
+				+ (i)
+				+ ".jpg'><img src='../../assets/images/portfolio/thumb/"
+				+ (i)
+				+ ".jpg'></a>"
+			)
+	    if (displayNames === true) {
+		  $("#container").append(
+				"<p class='t-title-etc'>"
+				+ artist.work[artist.work.length-(i)]
+				+ "</p>"
+	  		)
+		} else {
+		  $("#container").append(
+				"<br><br><br>"
+ 			)
+	    }
+	}
+}
+
+function loadCV() {
+	for(i = artist.cv.length; i > 0 ; i--){
+		$("#cvContainer").append(
+			"<p class='cv-item'>"
+			+ artist.cv[artist.cv.length-(i)]
+			+ "</p>"
+		)
+	}
+}
